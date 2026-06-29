f6e14fecc761ff672479a3224d345603e7550a70
Pawan Game <pavangame2003@gmail.com>
2026-06-30 00:01:50 +0530

    visitors feature removed

--- assets/js/script.js
+++ assets/js/script.js
@@ -82,17 +82,3 @@ modal.addEventListener("click", (e) => {
         modal.style.display = "none";
     }
 });
-
-
-
-
-// Visitor Counter
-fetch("https://api.countapi.xyz/hit/pawan-game/portfolio")
-    .then(response => response.json())
-    .then(data => {
-        document.getElementById("visitor-count").innerText = data.value;
-    })
-    .catch(error => {
-        console.error(error);
-        document.getElementById("visitor-count").innerText = "--";
-    });
